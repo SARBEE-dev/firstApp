c52ec6a3b9704717fc9a58241d7551e8e0856108
SALAUDEEN <68768976+SARBEE-dev@users.noreply.github.com>
2021-09-19 03:00:32 +0100

    Update urls.py

--- Todo/urls.py
+++ Todo/urls.py
@@ -15,11 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.models import TodoResource
-todo_resource = TodoResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Todo_app.urls')),
-path('api/', include(todo_resource.urls)),
 ]
