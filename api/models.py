86d06a448b3cc339a3e9eaed73abc6fba0d7527a
SALAUDEEN <68768976+SARBEE-dev@users.noreply.github.com>
2021-09-19 02:57:54 +0100

    Delete api directory

--- api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from tastypie.resources import ModelResource
-from Todo_app.models import Todo
-# Create your models here.
-
-class TodoResource(ModelResource):
-    class Meta:
-        queryset = Todo.objects.all()
-        resource_name = 'todo'
